faed12affb3a5ba97c64ed400d3f3fba37dead23
Chase Lamkin <chaselamkin@Chases-MacBook-Pro.local>
2023-02-11 13:41:01 -0600

    User moves complete

--- board.py
+++ board.py
@@ -23,6 +23,58 @@ class Board:
             print('Parameters (H == 1 or H == 0)')
             self.H = int(input('Please enter a new starting player: '))
 
+        self.createBoards()
+        self.startGame()
+
+    def createBoards(self):
+        self.board = [[' ' for j in range(self.N)] for i in range(self.N)]
+        self.value_board = [[0 for j in range(self.N)] for i in range(self.N)]
+        self.board_eval = 0
+        self.game_over = False
+    
+    def printBoard(self):
+        print(self.board)
+        for i in self.board:
+            print('+' + '---+' * self.N)
+            index = 1
+            for j in i:
+                print('|' + ' ' + j, end = ' ')
+                if index == self.N:
+                    print('|')
+                else:
+                    index += 1
+        print('+' + '---+' * self.N)
+
+    def startGame(self):
+        while not self.game_over:
+            #Users turn
+            if self.H == 0:
+                self.printBoard()
+                self.turn = int(input('Please enter the column (int) for your move: '))
+                self.check_move()
+                self.place_move()
+            #Computers turn
+            elif self.H == 1:
+                print('hi')
+            #error
+            else:
+                print('User variable H error: ending game')
+                self.game_over = True
+                return
+
+    def check_move(self):
+        while self.turn < 1 or self.turn > self.N or self.board[0][self.turn - 1] != ' ':
+            self.turn = int(input('That is an invalid column please try again: '))
+    
+    def place_move(self):
+        
+            
+
+
+            
+
+        
+
         
     
    
