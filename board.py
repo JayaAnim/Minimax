eace96a2909f22ed71eaeb35905de0e781bfe9e3
Pham2000 <ptstar2000@gmail.com>
2023-02-12 04:05:27 -0600

    I made some changes

--- board.py
+++ board.py
@@ -47,10 +47,10 @@ class Board:
 
     def startGame(self):
         while not self.game_over:
+            self.printEvaluation()
             #Users turn
             if self.H == 0:
                 self.printBoard()
-                #self.boardEvalutation() Tri Pham
                 self.turn = int(input('Please enter the column (int) for your move: '))
                 self.check_move()
                 self.place_move()
@@ -71,7 +71,7 @@ class Board:
         #testing if it work, Tri Pham
         print("place move")
 
-    #Tri Pham
+    #make value board
     def boardEvalutation(self):
         #produce board for evaluation
         mid = self.N//2
@@ -79,16 +79,14 @@ class Board:
             for j in range(self.N):
                 self.value_board[i][j] = (self.M - abs(i-mid)) + (self.M - abs(j-mid))
         
+    #print value board
+    def printEvaluation(self):
+        self.boardEvalutation()
+        print("Value board")
         for i in self.value_board:
-            print('+' + '---+' * self.N)
-            index = 1
-            for j in i:
-                print('|' + ' ' + j, end = ' ')
-                if index == self.N:
-                    print('|')
-                else:
-                    index += 1
-        print('+' + '---+' * self.N)
+            for g in i:
+                print("{:>3}".format(g), end="")
+            print()            
         
     
             
